40beca7211e1e9ab467ecda2f3e6706fec650cf9
sifilitico <sifilitico@hotmail.com>
2013-10-30 19:12:04 +0100

    Create LeerNumeros.java

---
+++ LeerNumeros.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.Scanner;
+
+class LeerNotas {
+	public static void main(String[] arg) {
+		File archivo = null;
+		FileReader fr = null;
+		BufferedReader br = null;
+		Scanner sc = new Scanner(System.in);
+		int i=1, total=1, np=0 ;
+		double vNota=0, suma=0, media=0;
+		String nota;
+		
+		System.out.println("Introduce la ruta del archivo (Ej: C:/) ");
+		String ruta = sc.nextLine();
+		System.out.println("Introduce el nombre del archivo a leer (acabado en .txt): ");
+		String nombreArchivo = sc.nextLine();
+		
+		try {
+			//Ruta: I:\\Programas\\EclipsePortable\\Data\\workspace\\lecturaEscritura\\src\\
+			archivo = new File(ruta+nombreArchivo);
+			fr = new FileReader(archivo);
+			br = new BufferedReader(fr);
+			String linea;
+			while ((linea = br.readLine()) != null){
+				nota=linea.substring((linea.length()-3), linea.length());
+				if(nota.contains("NP")){
+					System.out.println(linea);
+					System.out.println(" -> No presentado nº "+np);
+					np++;
+				}
+				else
+				{
+					System.out.println(linea);
+					vNota=Double.parseDouble(nota);
+					suma=suma+vNota;
+					System.out.println(" -> Nota: "+vNota+", Nota Ac.: "+suma);
+				}
+				total++;
+			}
+			System.out.println("Total presentados: "+total);
+			media=suma/total;
+			System.out.println("Nota media: "+media);			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (null != fr) {
+					fr.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+}
